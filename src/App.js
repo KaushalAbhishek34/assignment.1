87fb4469db463870d5945af39128191534294696
abhishek kaushal <abhikaushalak17@gmail.com>
2023-12-28 11:29:44 +0530

    12233

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import ProductList from './component/productlist';
+import ProductList from './component/Productlist';
 import ProductDetail from './component/ProductDetail'
 import './App.css';
 
